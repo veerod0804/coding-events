934b69c40b62c87b5209da0ad0b8f22f5b7a013f
Victoria <veerod0804@yahoo.com>
2022-10-10 12:11:55 -0500

    Completed model validation chapter

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -3,6 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 public class HomeController {
 
@@ -10,4 +13,5 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
 }
